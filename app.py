ece243777abcb27d080ca36415f0ab19b14a22a0
Alejandro Hipólito Hernando <alejandroohipolito@gmail.com>
2023-08-20 12:33:37 +0200

    skeleton of models, done

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from enum import Enum
 
-# Crear una instancia de la aplicación Flask
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
+db = SQLAlchemy(app)
+# db.init_app(app) IN BIGGER PROJECTS
 
-# Ruta de inicio (página principal)
-@app.route('/')
-def home():
-    return "¡Bienvenido a mi aplicación Flask!"
 
-if __name__ == '__main__':
-    app.run()
+
+class User_role(Enum):
+    COMMON_USER = 'common_user'
+    ADMIN = 'admin'
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(40), nullable=True)
+    email = db.Column(db.String(40), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    avatar = db.Column(db.String(200), nullable=True)
+    role = db.Column(db.Enum(User_role), nullable = False, default=User_role.COMMON_USER)
+
+
+
+class Pet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = 
+
+
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
